45069c753f850fcc604783b6ae6790f01452c959
Felipe Gomes <152811385+FelipeGomes97@users.noreply.github.com>
2024-06-23 18:25:36 +0100

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,31 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.minha-foto1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 100,
+    origin: 'right'
+})
+
+revelar.reveal('.apresentacao1',
+{
+        duration: 2000,
+        distance: '90px',
+        delay: 100
+})
+
+revelar.reveal('.habilidades',
+{
+            duration: 2000,
+            distance: '90px',
+            origin: 'right'
+})
+
+revelar.reveal('.deslizar',
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'left'
+})
+    
